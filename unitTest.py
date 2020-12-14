4b463568cdbb09c68f825990e34ec4a2ca276184
W.W <ww@example.com>
2020-12-14 17:39:04 +0800

    [Selenium][unittest] automatically new a table

--- unitTest.py
+++ unitTest.py
@@ -17,9 +17,11 @@ class Test(unittest.TestCase):
         self.browser = webdriver.Chrome(executable_path=self.config['Location']['webdriver'])
 
     def test_search(self):
-        print('testing...')
-        self.browser.get('https://www.google.com')
+        print('test_search is testing...')
+        self.browser.get(self.config['Web']['web_google'])
         self.browser.maximize_window()
+
+        sleep(1)
 
         searchColumn = self.browser.find_element_by_name('q')
         searchColumn.send_keys('one piece')
@@ -27,10 +29,49 @@ class Test(unittest.TestCase):
         #searchBtn = self.browser.find_element_by_name('btnK')
         #searchBtn.send_keys(Keys.RETURN)
 
+    def newTable(self, tableName='google'):
+
+        self.browser.execute_script('window.open()')
+        self.browser.switch_to_window(self.browser.window_handles[-1])
+        self.browser.get(self.config['Web']['web_{}'.format(tableName)])
+
+        sleep(2)
+
+    def closeTable(self):
+        print('@@1 window handles:', self.browser.window_handles)
+        self.browser.close()
+        print('@@2 window handles:', self.browser.window_handles)
+        sleep(1)
+        self.browser.switch_to_window(self.browser.window_handles[-1])
+        sleep(1)
+
+    def test_newTab(self):
+        print('test_multiWebpage is testing...')
+        self.test_search()
+
+        current_window = self.browser.current_window_handle
+        print('@@ current window handle id:', current_window)
+
+        sleep(1)
+
+        # New tables
+        #self.browser.execute_script('window.open(\'https://www.python.org/\')')
+        self.newTable('youtube')
+        self.newTable('python')
+        self.newTable('yahoo')
+
+        # Close tables from tail
+        self.closeTable()       # close yahoo
+        self.closeTable()       # close python
+
+        #self.browser.switch_to_window(self.browser.window_handles[0])        
+        self.browser.switch_to_window(current_window)        
+
     def tearDown(self):
         print('Do something after test')
         sleep(3)
         #self.browser.close()
 
 if __name__ == '__main__':
-    unittest.main()    
+    unittest.main()
+
